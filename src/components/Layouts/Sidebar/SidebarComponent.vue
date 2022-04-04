205911044a26ea0fd00fc765994960953129a7e8
Husen <husen@softwareseni.com>
2022-04-04 09:45:17 +0700

    add add app layout

---
+++ src/components/Layouts/Sidebar/SidebarComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-list-item>
+    <v-list-item-avatar>
+      <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>John Leider</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+
+  <v-divider></v-divider>
+
+  <v-list dense>
+    <v-list-item v-for="item in items" :key="item.title" link>
+      <v-list-item-icon>
+        <v-icon large color="blue-grey darken-2">{{ item.icon }}</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "SidebarComponent",
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+        { title: "Home", icon: "mdi-view-dashboard" },
+      ],
+    };
+  },
+  created() {
+    console.log({ items: this.items });
+  },
+};
+</script>
